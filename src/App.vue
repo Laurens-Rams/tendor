b56e47dcdd15cd510c2308b52bdf0cd19b191d6e
Laurens Art Ramsenthaler <artoflaurens@gmail.com>
2022-11-16 09:13:32 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import RickMortyFacts from './components/Cards/RickMortyFacts.vue';
+
+</script>
+
+<template>
+  <Suspense>
+    <template #fallback>loading..</template>
+    <RickMortyFacts />
+  </Suspense>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Cards/RickMortyFacts.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+const response = ref({})
+
+// RICK AND MORTY API - working
+let API = `https://rickandmortyapi.com/api`
+
+// ARTICLE API - working
+// let API =  https:newsapi.org/v2/top-headlines?q=Apple&from=2022-11-15&sortBy=popularity&apiKey=3e54c57a83a04c1d87d2d144e53cec01
+
+// CITY API - working
+// let API = https://api.teleport.org/api/cities/
+
+function getPromise(parameter) {
+	return fetch(API + parameter)
+}
+
+
+await new Promise(resolve => {
+    setTimeout(() => { 
+      resolve()
+    }, 1000)
+  })
+
+  try {
+    const res = await getPromise('/character')
+    response.value = (await res.json())
+    console.log(response.value.results)
+  } catch (error) {
+    response.value = {answer: 'The Rick API responded with an Error: ' + error}
+  }
+
+</script>
+
+<template>
+<p>{{ response }}</p>
+</template>
