f2fda011a07c1bd1962f9eecf1e1361aabdc8062
Laurens Art Ramsenthaler <artoflaurens@gmail.com>
2022-11-17 12:00:02 +0100

    routers

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
-<script setup>
-import { ref, onErrorCaptured } from 'vue'
-import List_comp from './components/Card/List_comp.vue';
-import article_com from './components/Card/Article.vue/article_com.vue';
-
-const error = ref('')
-
-const reload = () => {
-  window.location.reload()
-}
-
-onErrorCaptured((e) => {
-  console.log('error', e)
-  error.value = e
-  return true
-})
-
-</script>
-
 <template>
-  <div class="middle_box">
-  <Suspense>
-    <template #fallback>loading..</template>
-    <List_comp />
-  </Suspense>
-</div>
+  <header>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/About">About</router-link>
+    </nav>
+  </header>
+  <main>
+    <router-view></router-view> 
+  </main>
 </template>
-<style scoped>
 
-</style>
---
+++ src/views/Homepage.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Home</h1>
+    <main></main>
+</template>
